8aa17a39ba3c2d8e597873b2fda52d57940004d1
Bohdan Baran <bohdanbrn@gmail.com>
2017-03-13 14:37:48 +0200

    ver. 0.15

--- wp-content/themes/lvivin/functions.php
+++ wp-content/themes/lvivin/functions.php
@@ -10,7 +10,9 @@
 	add_image_size('300x200', 300, 200, true); // додаємо ще один розмір картинок 300x200 з обрізанням
 
 	function short_post_desc( $charlength ) {        //function for display short content for posts
-	    $excerpt = get_the_content();
+	    //$excerpt = get_the_content();
+	    global $post;
+	    $excerpt = get_post_meta( $post->ID, 'опис', true );
 	    if ( mb_strlen( $excerpt ) > $charlength ) {
 	        $subex = mb_substr( $excerpt, 0, $charlength );
 	        return $subex . '...';
@@ -20,7 +22,7 @@
 	    }
 	}
 
-	function short_post_title( $charlength, $post_id = null ) {        //function for display short title for posts
+	function short_post_title( $charlength ) {        //function for display short title for posts
 	    $excerpt = get_the_title( $post_id );
 	    $excerpt = trim( $excerpt );
 	    if ( mb_strlen( $excerpt ) > $charlength ) {
